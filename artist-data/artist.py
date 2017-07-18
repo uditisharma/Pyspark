742273c55c309b275117377d0613f3c32315495c
Dwarakanath Menon <dwaru@Dwarakanaths-MacBook-Air.local>
2017-07-18 16:02:41 +0100

    added artist2.py

--- artist-data/artist.py
+++ artist-data/artist.py
@@ -5,7 +5,9 @@ sc= SparkContext(conf=conf)
 lines=sc.textFile("/Users/dwaru/uditi-work/Pyspark/artist-data/unique_tracks.txt")
 name=lines.map(lambda x: (x.split("<SEP>")[2],1))
 count = name.reduceByKey(lambda x,y: x+y)
-swapped = count.map()
-results = count.take(10)
+swapped = count.map(lambda (x,y):(y,x))
+swapped_reversed = swapped.sortByKey(ascending=False)
+out = swapped_reversed.map(lambda (x,y):(y,x))
+results = out.take(10)
 for result in results:
-    print result
+    print result[0],result[1]
